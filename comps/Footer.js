06e572c72259704a864b5d60677f4b4f4b11f97a
Chris Fredricksen <cmfredricksen@gmail.com>
2022-03-30 16:31:12 -0500

    through lesson 6

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            <p>&copy; 2022 CMFRED</p>
+        </footer>
+     );
+}
+ 
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+const About = () => {
+    return ( 
+        <div>
+
+        <h1>About</h1>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus ipsam eum porro non saepe odit delectus enim a, quae hic quasi quibusdam culpa commodi natus, tempora minus eveniet expedita aliquam? Dolorem dignissimos pariatur unde ea reiciendis praesentium odit eaque repellat magni, perferendis architecto minus in magnam accusantium rem sed reprehenderit.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus ipsam eum porro non saepe odit delectus enim a, quae hic quasi quibusdam culpa commodi natus, tempora minus eveniet expedita aliquam? Dolorem dignissimos pariatur unde ea reiciendis praesentium odit eaque repellat magni, perferendis architecto minus in magnam accusantium rem sed reprehenderit.</p>
+        </div>
+     );
+}
+ 
+export default About;
